e47e785aa39a74d7cfce1c8c792420956e6d5cd0
Hamim-Reza <139302572+Hamim-Reza@users.noreply.github.com>
2023-09-12 08:27:27 +0600

    Update Find pairs of amicable numbers.c

--- Find pairs of amicable numbers.c
+++ Find pairs of amicable numbers.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main() {
-    int number1,number2,sum1 = 0,sum2 = 0;
-    scanf("%d%d",&number1,&number2);
-    for (int i = 1; i<number1; i++) {
-        if (number1 % i == 0) sum1 += i;
+    int num1,num2,sum1 = 0,sum2 = 0;
+    scanf("%d%d",&num1,&num2);
+    for (int i = 1; i<num1; i++) {
+        if (num1 % i == 0) sum1 += i;
     }
-    for (int i = 1; i<number2; i++) {
-        if (number2 % i == 0) sum2 += i;
+    for (int i = 1; i<num2; i++) {
+        if (num2 % i == 0) sum2 += i;
     }
-    if (sum1 == number2 && sum2 == number1)printf("Amicable\n");
+    if (sum1 == num2 && sum2 == num1)printf("Amicable\n");
     else printf("Not Amicable\n");
     return 0;
 }
